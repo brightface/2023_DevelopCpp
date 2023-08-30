acefd46dfed503269be1c2dd1519c4c8f4984754
brightface <brightface@hanmail.net>
2023-08-31 02:02:08 +0900

    add 플로이드에 오타

---
+++ 플로이드오타/플로이드오타/플로이드오타.cpp
@@ -0,0 +1,27 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#define WRONG { printf("WA"); exit(1); }
+#define CORRECT {printf("AC"); exit(0); }
+using namespace std;
+
+const int MX = 105;
+
+int D[MX][MX], E[MX][MX];
+
+int main()
+{
+	
+		int n = 100;
+		cout << n << '\n';
+		for (int y = 0; y < n; y++) {
+			for (int x = 0; x < n; x++) {
+				if (x == y) cout << 0 << ' ';
+				else if (y == n - 1 || x == n - 1) cout << 4999 << ' ';
+				else cout << 10000 << ' ';
+			}
+			cout << endl;
+		}
+	
+}
