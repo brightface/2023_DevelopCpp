cd03927c642cb799a6f70a03d5c8632a42d79514
brightface <brightface@hanmail.net>
2023-09-13 03:00:44 +0900

    Create 마법사상어와파이어스톰.py

---
+++ 마법사상어와 파이어스톰/마법사상어와파이어스톰.py
@@ -0,0 +1,75 @@
+from collections import deque
+n, q = map(int,input().split())
+n = 2 ** n
+arr =[]
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+l_list = list(map(int,input().split()))
+
+new = arr
+dy = [0,0,1,-1]
+dx = [1,-1,0,0]
+check = [[0] * n for _ in range(n)]
+count = [0] * (n+1)
+cnt = 1
+
+def rotate(L):
+    jump_size = 2 ** L
+    for y in range(0, n, jump_size):
+        for x in range(0, n, jump_size):
+            for i in range(jump_size):
+                for j in range(jump_size):
+                    new[y+j][x+jump_size - i - 1] = arr[y+i][x+j]
+
+def melt():
+
+    global new
+    for y in range(n):
+        for x in range(n):
+            c = 0
+            for i in range(4):
+                ny = y + dy[i]
+                nx = x + dx[i]
+                if 0 <= ny < n and 0 <= nx < n:
+                   if new[ny][nx] > 0:
+                    c += 1
+            if c < 3 and new[y][x] > 0:
+                new[y][x] -= 1
+
+def bfs(y,x, cnt):
+    global check
+    global new
+    q = deque()
+    q.append([y,x])
+    ss = 0
+    while q:
+        cy , cx = q.popleft()
+        for i in range(4):
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0 <= ny < n and 0 <= nx < n:
+                if new[ny][nx] > 0 and check[ny][nx] == 0:
+                    check[ny][nx] = cnt
+                    ss += 1
+                    q.append([ny,nx])
+    return ss
+def sum():
+    s = 0
+    for y in range(n):
+        for x in range(n):
+                s += new[y][x]
+    return s
+if __name__ == "__main__":
+    for i in l_list:
+        rotate(i)
+        melt()
+    print(sum())
+    for i in range(n):
+        for j in range(n):
+            if check[i][j] == 0 and new[i][j] > 0:
+                count[cnt] += 1
+                check[i][j] = 1
+                count[cnt]  S+= bfs(i,j,cnt)
+                cnt += 1
+    print(max(count))
+
